77e950fc64a86842dfdd059f5ba730ef1912ed95
Tukson <santiagonegreyra@gmail.com>
2023-08-13 21:44:19 -0300

    END

--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -63,7 +63,7 @@ function RegisterPage() {
                             <p className='text-red-500'>Password is required</p>
                     )}
 
-                    <button type='submit'>Register</button>
+                    <button type='submit' className='bg-sky-500 text-white px-4 py-2 rounded-md my-2'>Register</button>
                 </form>
 
                 <p className="flex gap-x-2 justify-between">
